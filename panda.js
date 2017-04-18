29d8e8380d193c97be617b9f301aa58bb0fa9508
nelyflores <nely.afloresr@gmail.com>
2017-04-18 11:03:15 +0000

    borrar imagenes

--- panda.js
+++ panda.js
@@ -1,2 +1,11 @@
-var img=getElementsByClassName('imagen');
+var img=document.getElementsByClassName("imagen");
 console.log(img);
+var longitud=img.length;
+
+for(var i=0;i<longitud; i++){
+		img[i].addEventListener( 'click', borrar);
+	}
+	function borrar () {
+		this.style.display = "none";
+		
+	}
